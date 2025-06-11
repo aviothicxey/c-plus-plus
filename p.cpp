2221fceeaabaeff4dac68300d1483a7576eba1e7
Aparna <gargaparna78@gmail.com>
2025-06-11 23:03:35 +0530

    init

---
+++ p.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout << "Hello, World!" << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin >> x;
+//     cout<<"you enterted:\t"<< x <<"\there";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string x;
+//     cout<<"enter a string: ";
+//     cin>> x;
+//     cout<< "length of string: "<<x.length();
+// return 0;
+// }
+
