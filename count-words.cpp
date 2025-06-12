654e3dad46ef566d19c58610bc523dff65149339
Aparna <gargaparna78@gmail.com>
2025-06-12 13:32:49 +0530

    count words

---
+++ count-words.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    int wordcount =1 ;
+    cout<<"enter a sentence: ";
+    getline(cin,str);
+    for(int i = 0; i< str.length();i++){
+        if (str[i]== ' '&& str[i+1]!= ' '){
+            wordcount++;
+        }
+    }
+    cout<<"wordcount:"<<wordcount<<endl;
+    return 0;
+}
