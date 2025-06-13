218f1e342d565c62afe1830a4b9bc29c4cecc396
Aparna <gargaparna78@gmail.com>
2025-06-13 18:40:59 +0530

    init

--- p.cpp
+++ p.cpp
@@ -24,3 +24,19 @@
 // return 0;
 // }
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int ages[]={20,22,18,35,48,26,87,70};
+    float avg,sum=0;
+    int i;
+    int length= sizeof(ages)/sizeof(ages[0]);
+    for(int i  : ages){
+        sum = sum + i;
+    }
+    avg = sum / length;
+    cout << "Average age is: " << avg << endl;
+   
+    return 0;
+}
--- pattern printing.cpp
+++ pattern printing.cpp
@@ -15,3 +15,41 @@ int main() {
 
     return 0;
 }
+/*
+input: 5
+Output:
+a
+ab
+abc
+abcd
+abcde 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << str[j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+Input: abcde
+Output:
+a
+ab
+abc
+abcd
+abcde
+*/
