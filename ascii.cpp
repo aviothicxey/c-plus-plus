31caacfeedcf3b4d599b5633eec7aa52c4dce480
Aparna <gargaparna78@gmail.com>
2025-06-14 16:54:12 +0530

    init

---
+++ ascii.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char x;
+    cout << "Enter a character: ";
+    cin >> x;
+    cout<< "ascii of x :"<<x<< " is "<<int(x);
+    return 0;
+}
