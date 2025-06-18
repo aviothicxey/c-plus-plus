1e7151de7f335cdbc38e834595815d71e86c5de0
Aparna <gargaparna78@gmail.com>
2025-06-18 18:22:35 +0530

    init

---
+++ array.cpp
@@ -0,0 +1,23 @@
+// Search in Array and Stop at First Match (Use break):
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of elements in array:";
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"enter the number to search:";
+    cin>>target;
+    for (int i = 0; i < n; i++){
+        if (arr[i]== target ){
+            cout<< "found at index"<< i << endl;
+            break;
+        }
+    }
+    return 0;
+
+}
