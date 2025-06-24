d83f344b034623f77bc71001d73963f72a5245e1
Aparna <gargaparna78@gmail.com>
2025-06-24 20:48:12 +0530

    init

---
+++ checkVowel.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+char ifVowel(char ch) {
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        return 'v'; 
+    } else {
+        return 'c'; 
+    }
+}
+
+int main() {
+    char chr;
+    cout << "Enter the letter: ";
+    cin >> chr;
+
+    char y = ifVowel(chr);
+
+    if (y == 'v') {
+        cout << "This is a vowel." << endl;
+    } else {
+        cout << "This is a consonant." << endl;
+    }
+
+    return 0;
+}
