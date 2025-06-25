2fe66c9d51b1fcc6205233ec066cecb04e9cb37e
Aparna <gargaparna78@gmail.com>
2025-06-25 21:48:47 +0530

    init

--- armstrong.cpp
+++ armstrong.cpp
@@ -31,3 +31,51 @@ int main() {
 
     return 0;
 }
+
+//display armstrong number between two intervals
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Function to check if a number is Armstrong
+bool isArmstrong(int num) {
+    int original = num;
+    int sum = 0;
+    int digits = 0;
+    int temp = num;
+
+    // Count number of digits
+    while (temp != 0) {
+        temp /= 10;
+        digits++;
+    }
+
+    temp = num;
+    while (temp != 0) {
+        int digit = temp % 10;
+        sum += pow(digit, digits);
+        temp /= 10;
+    }
+
+    return sum == original;
+}
+
+int main() {
+    int low, high;
+    cout << "Enter lower limit: ";
+    cin >> low;
+    cout << "Enter upper limit: ";
+    cin >> high;
+
+    cout << "Armstrong numbers between " << low << " and " << high << " are:\n";
+    for (int i = low; i <= high; i++) {
+        if (isArmstrong(i)) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
