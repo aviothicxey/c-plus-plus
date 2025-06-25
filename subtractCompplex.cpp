cb8b33620ff6ceeeffc13660d6c3f104449bb6e3
Aparna <gargaparna78@gmail.com>
2025-06-25 18:24:06 +0530

    init

--- subtractCompplex.cpp
+++ subtractCompplex.cpp
@@ -1,14 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    float r1 ,i1 ,r2 ,i2 , reR , imR;
-    cout << " Complex number 1 : "<<r1<<"+"<<i1 << "i";
-    cin >> r1 >> i1 ;
-    cout << "Complex number 2 : "<<r2<<"+"<<i2 << "i";
-    cin>> r2 >> i2;
-    reR = r1 - r2;
-    imR = i1 - i2 ;
-    cout << "subtraction : "<< reR <<"+" << imR ;
-    return 0;
 
+int main() {
+    float r1, i1, r2, i2, reR, imR;
+
+    cout << "Complex number 1 (real and imaginary): ";
+    cin >> r1 >> i1;
+
+    cout << "Complex number 2 (real and imaginary): ";
+    cin >> r2 >> i2;
+
+    reR = r1 - r2;
+    imR = i1 - i2;
+    cout << "Subtraction: " << reR;
+    if (imR >= 0)
+        cout << " + " << imR << "i" << endl;
+    else
+        cout << " - " << -imR << "i" << endl;
+
+    return 0;
 }
