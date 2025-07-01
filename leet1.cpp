5ee446d1ca3ecc8672bf8f3a2fd09f5b30079f39
Aparna <gargaparna78@gmail.com>
2025-07-01 17:27:57 +0530

    init

---
+++ leet1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i <= n; i++){
+        cout<<"element "<< i << "=" <<"\n";
+        cin >> arr[i];
+    }
+    int target ;
+    cin >> target;
+    for ( int i = 0 ; i < n ; i++){
+        for (int j = i+ 1 ; i < n ; j++ ){  // j starts from i+1 to avoid repeating and self-sum
+            if ( arr[i] + arr[j] == target){  
+            cout<<"["<<i << ", " << j << "]";
+            return 0;
+            }
+        }
+    }
+    return 0;
+}
