9a2bbf5e280cca96a74ba4801c2e9ccd78a78bd9
Aparna <gargaparna78@gmail.com>
2025-07-08 14:59:26 +0530

    init

--- checkPrime.cpp
+++ checkPrime.cpp
@@ -1,3 +1,4 @@
+/*
 //USing functions:
 #include <iostream>
 using namespace std;
@@ -17,4 +18,26 @@ int main(){
     else
     cout<< num <<"is not prime.";
     return 0;
+}
+*/
+
+//Without Function:
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter  number:";
+    cin >> n;
+    if (n==2 || n == 3 || n == 5){
+        cout<< n << "is prime .";
+    }
+    
+    else if(n% 2 ==0 ||n%3 == 0 || n % 5 == 0 ){
+        cout<< n <<" is not prime.";
+    }
+    else{
+         cout<< n << "is prime .";
+    }
+    return 0;
+
 }
