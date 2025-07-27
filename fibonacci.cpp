8a5c256981490da594a1c79adf7b1d31c0bdd382
Aparna <gargaparna78@gmail.com>
2025-07-27 19:26:33 +0530

    init

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,18 +1,40 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n ;
+//     cout << "enter the number of terms : ";
+//     cin >> n ;
+//     int first = 0 ; int second = 1;
+//     int next ;
+//     cout << "FIBONACCI SEQUENCE : ";
+//     for (int i = 0 ; i< n ; i++){
+//         cout << first << " ";
+//         next = first + second ;
+//         first = second ;
+//         second = next ;
+//     }
+//     cout << endl ;
+//     return 0;
+// }
+
+
+//FIBBONACCI USING CLASSES:
+
+#include <iostream>
 using namespace std;
-int main(){
-    int n ;
-    cout << "enter the number of terms : ";
-    cin >> n ;
-    int first = 0 ; int second = 1;
-    int next ;
-    cout << "FIBONACCI SEQUENCE : ";
-    for (int i = 0 ; i< n ; i++){
-        cout << first << " ";
-        next = first + second ;
-        first = second ;
-        second = next ;
+class fibo{
+    public:
+    int fibbonaaci(int n){
+        if (n == 0)return 0;
+        if (n == 1 )return 1;
+        return fibbonaaci(n-1) + fibbonaaci(n-2);
     }
-    cout << endl ;
+};
+int main(){
+    fibo fib;
+    int n ;
+    cin >> n;
+    int result = fib.fibbonaaci(n);
+    cout<<result;
     return 0;
 }
