aa76772ff862b9173979eca9540a07e25ed866be
Aparna <gargaparna78@gmail.com>
2025-11-27 19:37:18 +0530

    init

--- practice.java
+++ practice.java
@@ -255,3 +255,48 @@ class merged{
 
 //ist and last occurence of an integer in a sorted list with duplicates:
 
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums , target);
+        int last = findLast(nums,target);
+        return new int[]{first,last};
+    }
+
+    private int findFirst(int [] nums ,int target){
+        int start = 0 ; int end = nums.length -1 ; int index = -1;
+        while( start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target ){
+                index = mid ;
+                end = mid -1;
+            }
+            else if ( target > nums[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
+        return index;
+    }
+    private int findLast(int nums[] , int target){
+        int  start = 0 ; int end = nums.length -1 ; int index = -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                index = mid;
+                start = mid +1;
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return index;
+    }
+}
+
+//find count of a number in a sorted list with duplicates:
+
