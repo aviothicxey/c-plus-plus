856cc029b27a6fa40543a03cadd9106ae7538ec8
Aparna <gargaparna78@gmail.com>
2025-12-06 16:23:43 +0530

    init

--- practice.java
+++ practice.java
@@ -728,3 +728,19 @@ class Result{
     
 }
 
+//Linked List:
+
+//print the list:
+static void forwardPrint(Node head) {
+Node temp = head ;
+    while(temp != null){
+        System.out.print(temp.data + "-");
+        temp = temp.next;
+    }
+}
+
+static void backwardPrint(Node head) {
+if(head == null)return ;
+    backwardPrint(head.next);
+    System.out.print(head.data + "-");
+}
