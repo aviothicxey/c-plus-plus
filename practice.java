332b4059d97332eed9ff5597eb6708c9b00c636f
Aparna <gargaparna78@gmail.com>
2025-12-10 00:15:45 +0530

    init

--- practice.java
+++ practice.java
@@ -979,3 +979,29 @@ public ListNode reverseBetween(ListNode head , int left , int right){
     return dummy.next;
 
 }
+
+// Add two Numbers:
+public Node addTwoNumbers(Node l1 , Node l2){
+    Node temp = new Node(0);
+    Node curr = temp;
+
+    int carry = 0;
+
+    while(l1 != null || l2 != null || carry != 0){
+        int sum = carry;
+
+        if(l1 != null){
+            sum+= l1.val;
+            l1 = l1.next;
+        }
+        if(l2!=null){
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        carry = sum / 10;
+        curr.next = new Node(sum% 10);
+        curr = curr.next;
+    }
+    return temp.next;
+}
