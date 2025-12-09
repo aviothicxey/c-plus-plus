d4324cdde1adb1bf32d4c17bda5da91087657f4c
Aparna <gargaparna78@gmail.com>
2025-12-09 17:49:37 +0530

    init

--- practice.java
+++ practice.java
@@ -918,3 +918,32 @@ public Node reverseList(Node head){
     }
     return head;
 }
+//leetcode 206 sol2 :
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev= null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }  
+        return prev;
+    }
+}
+
+//leetcode 206 sol3 :
+class Solution {
+    public ListNode reverseList(ListNode head) {
+    if(head == null || head.next == null) return head;
+
+    ListNode newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+
+    return newHead;
+}
+
+}
