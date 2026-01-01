f74e06fa58e7c596b14dfcad7d062f1275035c97
Aparna <gargaparna78@gmail.com>
2026-01-01 21:47:02 +0530

    init

--- DSA.java
+++ DSA.java
@@ -187,3 +187,35 @@ public class Solution{
         
     }
 }
+
+
+// Family Photograph (Reverse in Groups of k)
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+           arr[i] = sc.nextInt(); 
+        }
+        int k = sc.nextInt();
+ // i+=k ensures:Each group starts at i , Next group starts directly at i + k , No overlapping, no double-reversing
+        for(int i = 0 ; i < n ; i+=k){
+            int left = i ;
+            int right = Math.min(i+k -1 , n-1);
+/*left → start of group
+right → end of group
+Math.min protects the last group if size < k */
+            while(left < right){
+                int temp = arr[left];
+                arr[left]  = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        for(int x : arr) System.out.print(x + " ");
+    }
+}
