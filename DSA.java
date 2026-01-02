bace1fc5fd5b06eff6ca467559f951c3ab605566
Aparna <gargaparna78@gmail.com>
2026-01-02 19:18:40 +0530

    init

--- DSA.java
+++ DSA.java
@@ -427,3 +427,38 @@ class Main{
         }
     }
 }
+
+
+//day 2 :
+// K Challenges For Last Occurrence
+class Main{
+    static int lastOccurrence(int[] arr , int x){
+        int left = 0 ; int right = arr.length -1; int ans = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == x){
+                ans = mid ;
+                left = mid +1;
+            }
+            else if(arr[mid] < x) right = mid -1; //descending array
+            else left = mid + 1;
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int K = sc.nextInt();
+        long sum = 0;
+        for(int i = 0 ; i < K ;i++){
+            int x = sc.nextInt();
+            sum += lastOccurrence(arr,x);
+        }
+        System.out.println(sum);
+    }
+}
+
