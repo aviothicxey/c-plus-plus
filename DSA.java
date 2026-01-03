1b47dec5243257c752dc0f3e2021f05835ff78be
Aparna <gargaparna78@gmail.com>
2026-01-03 11:58:39 +0530

    init

--- DSA.java
+++ DSA.java
@@ -462,3 +462,28 @@ class Main{
     }
 }
 
+// Secure Key Exchange
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while(T-->0){
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            HashSet<Integer> set = new HashSet<>();
+            int[] arr = new int[N];
+            for(int i = 0 ; i < N ; i++){
+                arr[i] = sc.nextInt();
+                set.add(arr[i]);
+            }
+            int cnt = 0;
+            for(int x : arr){
+                if(set.contains(x+K)) cnt++;
+            }
+            System.out.println(cnt);
+        }
+    }
+}
