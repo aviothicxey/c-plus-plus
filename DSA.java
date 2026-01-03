1698d9e7ec2b6607673a1015af06784ce42a73d6
Aparna <gargaparna78@gmail.com>
2026-01-03 12:27:12 +0530

    init

--- DSA.java
+++ DSA.java
@@ -487,3 +487,63 @@ class Main{
         }
     }
 }
+
+// DataStream Log Audit (Frequency of K)
+// count the freq in sorted array:
+
+class Main{
+    private static int lastOccurrence(int[] arr , int target){
+        int low = 0; 
+        int high = arr.length -1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low )/2;
+            
+            if(arr[mid] == target){
+                ans = mid ;
+                low = mid + 1;
+            }else if (arr[mid] > target) high = mid -1;
+            else low = mid + 1;
+        }
+        return ans;
+    }
+    private static int firstOccurence(int[] arr , int target){
+        int low = 0; int high = arr.length -1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + ( high - low ) / 2;
+
+            if(arr[mid] == target){
+                ans = mid ;
+                high = mid -1;
+            }
+            else if (arr[mid] > target) high = mid -1;
+            else low = mid +1;
+        }
+        return ans;
+    }
+
+    private static int count(int[] arr , int target){
+        int first = firstOccurence(arr , target);
+        int last = lastOccurrence(arr,target);
+        if(first == -1) return 0;
+        else return last - first +1;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while(T-- > 0){
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            int[] arr = new int[N];
+
+            for(int i = 0 ; i < N ; i++){
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(count(arr,K));
+        }
+    }
+}
