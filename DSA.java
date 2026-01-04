e614c5246ee317a62f43583135a7a37600a62160
Aparna <gargaparna78@gmail.com>
2026-01-04 12:45:21 +0530

    init

--- DSA.java
+++ DSA.java
@@ -747,3 +747,23 @@ class Main {
         sc.close();
     }
 }
+
+// valid parentheses:
+
+class Solution{
+    public boolean isValid(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length();i++){
+            char ch = s.charAt(i);
+            if(ch = '[' || ch = '{' || ch = '(') st.push(ch);
+            else {
+                if(st.isEmpty())return false;
+                char c = st.peek();
+                st.pop();
+                if(ch = ']'&& c = '[' ||ch = '}'&& ch = '{' || ch = ')' && ch = '(')return true;
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
