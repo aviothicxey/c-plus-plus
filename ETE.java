b56a44cc66fb227ca21e22b8de86aca664013152
Aparna <gargaparna78@gmail.com>
2026-01-06 19:26:42 +0530

    init

--- ETE.java
+++ ETE.java
@@ -97,3 +97,31 @@ class Solution{
     }
 }
 
+// q5:
+class Main{
+    public static Node mergeList(Node l1 , Node l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        Node dummy = new Node(0);
+        Node temp = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if(l1 != null) temp.next = l1;
+        if(l2 != null) temp.next = l2;
+        return dummy.next;
+
+    }
+}
+
+// q6 
