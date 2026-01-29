01dc19b784402d5cac1c440091631f45c052a933
Aparna <gargaparna78@gmail.com>
2026-01-29 16:57:06 +0530

    init

--- Recursion.java
+++ Recursion.java
@@ -11,11 +11,48 @@ public class Recursion{
         System.out.println("Ayush");
         pn(i+1 , n);
     }
-
+// print numbers 1 to n:
+   static void printNum(int i , int n){
+    if( i > n ) return;
+    System.out.println(i);
+    printNum(i+1, n);
+   }
+// with backtracking:
+static void printn(int i , int n){
+    if( i < 1) return;
+    printn(i-1, n);
+    System.out.println(i);
+}
+static void bpn(int i , int n){
+    if(i > n) return;
+    bpn(i+1 , n);
+    System.out.println(i);
+}
+// sum of n numbers:(Paramtrized):
+static void sumOfN(int i , int sum){
+    if(i < 1){
+        System.out.println(sum);
+        return;
+    }
+    sumOfN(i-1, sum+i);
+}
+// sum of n numbers(functional):
+static int fun(int n){
+if(n == 0) return 0;
+return n+fun(n-1);
+}
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt() ;
-         printName(n); 
-         pn(1,n);
+         printName(n); // Aparna n times
+         pn(1,n); // ayush n times
+         printNum(1, n); //1 to n normal recursion
+         printn(n, n); //1 to n(backtracing)
+         bpn(1, n);//n to 1 (backtracking)
+         sumOfN(n,0);
+         int res = fun(n);
+         System.out.println(res);
+         sc.close();
     }
+    
 }
